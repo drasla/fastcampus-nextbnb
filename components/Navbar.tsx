c4d230a140274d5c72c7fcaf609495d2a77e837e
Tae.Garam [Office] <drasla@naver.com>
2024-03-21 16:19:38 +0900

    Ch8-5-2. react-calendar를 시용해 검색 필터 내 달력 컴포넌트 구현하기

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,6 +13,8 @@ import {
     AiOutlineUser,
 } from "react-icons/ai";
 import dayjs from "dayjs";
+import "dayjs/locale/ko";
+import Calendar from "react-calendar";
 
 const menus = [
     { id: 1, title: "로그인", url: "/users/login" },
@@ -256,7 +258,25 @@ export default function Navbar() {
                         </div>
                         <button
                             type="button"
-                            className="bg-rose-600 text-white rounded-full h-10 mx-4 sm:w-24 my-auto flex justify-center gap-1 px-3 py-2 hover:shadow hover:bg-rose-500"
+                            className={twMerge(
+                                "bg-rose-600",
+                                "text-white",
+                                "rounded-full",
+                                "h-10",
+                                "mx-4",
+                                "sm:w-24",
+                                "mt-4",
+                                "sm:mt-2",
+                                "md:mt-0",
+                                "my-auto",
+                                "flex",
+                                "justify-center",
+                                "gap-1",
+                                "px-3",
+                                "py-2",
+                                "hover:shadow",
+                                "hover:bg-rose-500",
+                            )}
                             onClick={() => {
                                 setShowFilter(false);
                                 setDetailFilter(null);
@@ -272,7 +292,7 @@ export default function Navbar() {
                     "grow",
                     "basis-0",
                     "hidden",
-                    "sm:flex",
+                    "md:flex",
                     "gap-4",
                     "align-middle",
                     "my-auto",
@@ -357,7 +377,26 @@ interface FilterComponentProps {
 
 const LocationFilter = ({ filterValue, setFilterValue, setDetailFilter }: FilterComponentProps) => {
     return (
-        <div className="absolute top-80 sm:top-[70px] border border-gray-200 px-8 py-10 flex flex-col bg-white w-full mx-auto inset-x-0 sm:max-w-3xl sm:w-[780px] rounded-xl">
+        <div
+            className={twMerge(
+                "absolute",
+                "top-80",
+                "sm:top-[70px]",
+                "border",
+                "border-gray-200",
+                "px-8",
+                "py-10",
+                "flex",
+                "flex-col",
+                "bg-white",
+                "w-full",
+                "mx-auto",
+                "inset-x-0",
+                "sm:max-w-3xl",
+                "sm:w-[640px]",
+                "md:w-[780px]",
+                "rounded-xl",
+            )}>
             <div className="text-sm font-semibold">지역으로 검색하기</div>
             <div className="flex flex-wrap gap-4 mt-4">
                 {["서울", "부산", "대구", "인천", "광주", "대전", "울산"]?.map(value => (
@@ -391,36 +430,86 @@ const LocationFilter = ({ filterValue, setFilterValue, setDetailFilter }: Filter
 };
 
 const CheckInFilter = ({ filterValue, setFilterValue, setDetailFilter }: FilterComponentProps) => {
+    const onChange = (e: any) => {
+        setFilterValue({
+            ...filterValue,
+            checkIn: dayjs(e).format("YYYY-MM-DD"),
+        });
+
+        setDetailFilter("checkOut");
+    };
+
     return (
-        <div className="absolute top-80 sm:top-[70px] border border-gray-200 px-8 py-10 flex flex-col bg-white w-full mx-auto inset-x-0 sm:max-w-3xl sm:w-[780px] rounded-xl">
+        <div
+            className={twMerge(
+                "absolute",
+                "top-80",
+                "sm:top-[70px]",
+                "border",
+                "border-gray-200",
+                "px-8",
+                "py-10",
+                "flex",
+                "flex-col",
+                "bg-white",
+                "w-full",
+                "mx-auto",
+                "inset-x-0",
+                "sm:max-w-3xl",
+                "sm:w-[640px]",
+                "md:w-[780px]",
+                "rounded-xl",
+            )}>
             <div className="text-sm font-semibold">체크인 날짜 설정하기</div>
-            <input
-                type="date"
-                className="mt-4 border border-gray-200 py-3 px-2.5 rounded-lg"
-                defaultValue={filterValue.checkIn}
-                min={dayjs().format("YYYY-MM-DD")}
-                onChange={e => {
-                    setFilterValue({ ...filterValue, checkIn: e.target.value });
-                    setDetailFilter("checkOut");
-                }}
+            <Calendar
+                className={twMerge("mt-8", "mx-auto")}
+                onChange={onChange}
+                minDate={new Date()}
+                defaultValue={filterValue.checkIn ? new Date(filterValue.checkIn) : null}
+                formatDay={(_, date) => dayjs(date).format("DD")}
             />
         </div>
     );
 };
 
 const CheckOutFilter = ({ filterValue, setFilterValue, setDetailFilter }: FilterComponentProps) => {
+    const onChange = (e: any) => {
+        setFilterValue({
+            ...filterValue,
+            checkOut: dayjs(e).format("YYYY-MM-DD"),
+        });
+
+        setDetailFilter("guest");
+    };
+
     return (
-        <div className="absolute top-80 sm:top-[70px] border border-gray-200 px-8 py-10 flex flex-col bg-white w-full mx-auto inset-x-0 sm:max-w-3xl sm:w-[780px] rounded-xl">
+        <div
+            className={twMerge(
+                "absolute",
+                "top-80",
+                "sm:top-[70px]",
+                "border",
+                "border-gray-200",
+                "px-8",
+                "py-10",
+                "flex",
+                "flex-col",
+                "bg-white",
+                "w-full",
+                "mx-auto",
+                "inset-x-0",
+                "sm:max-w-3xl",
+                "sm:w-[640px]",
+                "md:w-[780px]",
+                "rounded-xl",
+            )}>
             <div className="text-sm font-semibold">체크아웃 날짜 설정하기</div>
-            <input
-                type="date"
-                className="mt-4 border border-gray-200 py-3 px-2.5 rounded-lg"
-                defaultValue={filterValue.checkOut}
-                min={dayjs(filterValue.checkIn).add(1, "day").format("YYYY-MM-DD")}
-                onChange={e => {
-                    setFilterValue({ ...filterValue, checkOut: e.target.value });
-                    setDetailFilter("guest");
-                }}
+            <Calendar
+                className={twMerge("mt-8", "mx-auto")}
+                onChange={onChange}
+                minDate={filterValue.checkIn ? new Date(filterValue.checkIn) : new Date()}
+                defaultValue={filterValue.checkOut ? new Date(filterValue.checkOut) : null}
+                formatDay={(_, date) => dayjs(date).format("DD")}
             />
         </div>
     );
@@ -430,7 +519,26 @@ const GuestFilter = ({ filterValue, setFilterValue, setDetailFilter: _ }: Filter
     const [counter, setCounter] = useState<number>(filterValue.guest || 0);
 
     return (
-        <div className="absolute top-80 sm:top-[70px] border border-gray-200 px-8 py-10 flex flex-col bg-white w-full mx-auto inset-x-0 sm:max-w-3xl sm:w-[780px] rounded-xl">
+        <div
+            className={twMerge(
+                "absolute",
+                "top-80",
+                "sm:top-[70px]",
+                "border",
+                "border-gray-200",
+                "px-8",
+                "py-10",
+                "flex",
+                "flex-col",
+                "bg-white",
+                "w-full",
+                "mx-auto",
+                "inset-x-0",
+                "sm:max-w-3xl",
+                "sm:w-[640px]",
+                "md:w-[780px]",
+                "rounded-xl",
+            )}>
             <div className="text-sm font-semibold">게스트 수 추가하기</div>
             <div className="mt-4 border border-gray-200 rounded-lg py-2 px-4 flex justify-between items-center">
                 <div>
